47befcffdbebc31b92443338e2afb46e64d8831b
skanehira <sho19921005@gmail.com>
2022-01-11 00:07:27 +0900

    feat: implement beta

--- README.md
+++ README.md
@@ -6,22 +6,40 @@
 ![GitHub Release Status](https://img.shields.io/github/workflow/status/skanehira/languagetool-lint/Release?label=release)
 
 # languagetool-lint
-This is template that help you to quick implement some CLI using Go.
+Lint tool for [languagetool](https://github.com/languagetool-org/languagetool).
 
-This repository is contains following.
+## Requirements
+[languagetool](https://github.com/languagetool-org/languagetool).
+NOTE: You can also use [docker-languagetool](https://github.com/Erikvl87/docker-languagetool) to run `languagetool server`.
 
-- minimal CLI implementation using [spf13/cobra](https://github.com/spf13/cobra)
-- CI/CD
-  - golangci-lint
-  - go test
-  - goreleaser
-  - dependabot for github-actions and Go
-  - CodeQL Analysis (Go)
+## Use as a lint tool
+1. Run your `languagetool server` in local.
+2. Execute `languagetool-lint` like bellow.
+   ```sh
+   $ languagetool-lint -a http://localhost:8081 -l "en-US" your_text_file
+   your_text_file:2:27: The abbreviation “e.g.” (= for example) requires two periods.
+   ```
 
-## How to use
-1. fork this repository
-2. replace `skanehira` to your user name using `sed`(or others)
-3. run `make init`
+## Use as a LSP Server
+1. Install [efm-langserver](https://github.com/mattn/efm-langserver)
+2. Add config as bellow.
+   ```yaml
+   version: 2
+   tools:
+     languagetool-lint: &languagetool-lint
+       lint-command: 'languagetool-lint'
+       lint-ignore-exit-code: true
+       lint-stdin: true
+       lint-formats:
+         - '%f:%l:%c: %m'
+   languages:
+     markdown:
+       - <<: *languagetool-lint
+   ```
 
 ## Author
 skanehira
+
+## Thanks
+- [languagetool](https://github.com/languagetool-org/languagetool).
+- [docker-languagetool](https://github.com/Erikvl87/docker-languagetool)
--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,22 @@
 package cmd
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use: "languagetool-lint",
+	Args: func(cmd *cobra.Command, args []string) error {
+		return nil
+	},
 }
 
 func exitError(msg interface{}) {
@@ -16,9 +24,149 @@ func exitError(msg interface{}) {
 	os.Exit(1)
 }
 
+func init() {
+	rootCmd.PersistentFlags().StringP("addr", "a", "http://localhost:8010", "languagetool server host")
+	rootCmd.PersistentFlags().StringP("lang", "l", "en-US", "language")
+}
+
+type Rule struct {
+	ID          string `json:"id,omitempty"`
+	Description string `json:"description,omitempty"`
+}
+
+type Context struct {
+	Text   string `json:"text,omitempty"`
+	Offset int64  `json:"offset,omitempty"`
+	Length int64  `json:"length,omitempty"`
+}
+
+type Match struct {
+	Message      string  `json:"message,omitempty"`
+	ShortMessage string  `json:"short_message,omitempty"`
+	Context      Context `json:"context,omitempty"`
+	Offset       int64   `json:"offset,omitempty"`
+	Length       int64   `json:"length,omitempty"`
+	Sentence     string  `json:"sentence,omitempty"`
+	Rule         Rule    `json:"rule,omitempty"`
+}
+
+type Response struct {
+	Matches []Match `json:"matches,omitempty"`
+}
+
+type Annotation struct {
+	Text string `json:"text"`
+}
+
+type Request struct {
+	Annotation []Annotation `json:"annotation,omitempty"`
+}
+
+type Line struct {
+	Text string
+	Num  int64
+}
+
+func getpos(lines map[string]Line, c Context) (int64, int, error) {
+	word := c.Text[c.Offset : c.Offset+c.Length]
+	var lnum int64
+	var col int
+	for text, line := range lines {
+		if strings.Contains(text, word) {
+			lnum = line.Num
+			col = strings.Index(line.Text, word)
+			break
+		}
+	}
+	return lnum, col, nil
+}
+
+// format is format languagetool response to lint format
+// TODO improve algrithm for search word from sentences
+func format(fname string, lines map[string]Line, resp Response) ([]string, error) {
+	errors := make([]string, len(resp.Matches))
+	for i, m := range resp.Matches {
+		lnum, col, err := getpos(lines, m.Context)
+		if err != nil {
+			return nil, err
+		}
+		errors[i] = fmt.Sprintf("%s:%d:%d: %s", fname, lnum, col, m.Message)
+	}
+	return errors, nil
+}
+
 func Execute() {
 	rootCmd.Run = func(cmd *cobra.Command, args []string) {
-		_ = rootCmd.Help()
+		lang, err := rootCmd.PersistentFlags().GetString("lang")
+		if err != nil {
+			exitError(err)
+		}
+		addr, err := rootCmd.PersistentFlags().GetString("addr")
+		if err != nil {
+			exitError(err)
+		}
+
+		if lang == "" || addr == "" {
+			_ = rootCmd.Help()
+			return
+		}
+
+		var (
+			fname string
+			sc    *bufio.Scanner
+		)
+
+		// create request
+		if len(args) == 0 {
+			fname = "stdin"
+			sc = bufio.NewScanner(os.Stdin)
+		} else {
+			fname = args[0]
+			fr, err := os.Open(fname)
+			if err != nil {
+				exitError(err)
+			}
+			sc = bufio.NewScanner(fr)
+		}
+		var lnum int64 = 1
+		lines := make(map[string]Line)
+		var req Request
+		for sc.Scan() {
+			t := sc.Text()
+			lines[strings.TrimSpace(t)] = Line{
+				Text: t,
+				Num:  lnum,
+			}
+			req.Annotation = append(req.Annotation, Annotation{
+				Text: t + "\n",
+			})
+			lnum++
+		}
+
+		data, err := json.Marshal(req)
+		if err != nil {
+			exitError(err)
+		}
+		v := url.Values{}
+		v.Add("data", string(data))
+		v.Add("language", lang)
+
+		resp, err := http.DefaultClient.PostForm(addr+"/v2/check", v)
+		if err != nil {
+			exitError(err)
+		}
+		defer resp.Body.Close()
+		var langResp Response
+		if err := json.NewDecoder(resp.Body).Decode(&langResp); err != nil {
+			exitError(err)
+		}
+		result, err := format(fname, lines, langResp)
+		if err != nil {
+			exitError(err)
+		}
+		for _, l := range result {
+			println(l)
+		}
 	}
 
 	if err := rootCmd.Execute(); err != nil {
